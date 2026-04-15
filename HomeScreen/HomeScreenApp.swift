15d9be09dafdd5ad74bc656ccf9dc8e254ac60c0
Gaurang Pant <gaurangpant181@gmail.com>
2026-04-15 12:55:52 +0530

    Initial Commit

---
+++ HomeScreen/HomeScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeScreenApp.swift
+//  HomeScreen
+//
+//  Created by Gaurang Pant on 15/04/26.
+//
+
+import SwiftUI
+
+@main
+struct HomeScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
